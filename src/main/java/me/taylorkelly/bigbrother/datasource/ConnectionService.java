4e7ecce66188be328dbf93c7034312913e2fb32d
Daniel Chesters <daniel.chesters@gmail.com>
2011-02-12 01:41:04 +0100

    Revert "Revert "Remplace Vector by ArrayList and 2/3 others litle thinks""
    This reverts commit a6eaabc27b2131a2d2de2716b43e88104cddcf46.

--- src/main/java/me/taylorkelly/bigbrother/datasource/ConnectionService.java
+++ src/main/java/me/taylorkelly/bigbrother/datasource/ConnectionService.java
@@ -1,108 +1,102 @@
 package me.taylorkelly.bigbrother.datasource;
 
-/*
- * 
- */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Enumeration;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <b>Purpose:</b>Realizes a connection pool for all JDBC connections.<br>
- * <b>Description:</b>http://java.sun.com/developer/onlineTraining/Programming/JDCBook/
- * conpool.html<br>
+ * <b>Description:</b>http://java.sun.com/developer/onlineTraining/Programming/
+ * JDCBook/ conpool.html<br>
  * <b>Copyright:</b>Licensed under the Apache License, Version 2.0.
  * http://www.apache.org/licenses/LICENSE-2.0<br>
  * <b>Company:</b>SIMPL<br>
- * 
+ *
  * @author schneimi
  * @version $Id$<br>
  * @link http://code.google.com/p/simpl09/
  */
-public class ConnectionService {
-  private Vector<JDCConnection> connections;
-  private String url, user, password;
-  final private long timeout = 60000;
-  private ConnectionReaper reaper;
-  final private int poolsize = 10;
+public final class ConnectionService {
+	private List<JDCConnection> connections;
+	private String url, user, password;
+	private static final long TIMEOUT = 60000;
+	private ConnectionReaper reaper;
+	private static final int POOLSIZE = 10;
 
-  public ConnectionService(String url, String user, String password) {
-    this.url = url;
-    this.user = user;
-    this.password = password;
-    connections = new Vector<JDCConnection>(poolsize);
-    reaper = new ConnectionReaper(this);
-    reaper.start();
-  }
+	public ConnectionService(String url, String user, String password) {
+		this.url = url;
+		this.user = user;
+		this.password = password;
+		connections = new ArrayList<JDCConnection>(POOLSIZE);
+		reaper = new ConnectionReaper(this);
+		reaper.start();
+	}
 
-  public synchronized void reapConnections() {
-    long stale = System.currentTimeMillis() - timeout;
-    Enumeration<JDCConnection> connlist = connections.elements();
+	public synchronized void reapConnections() {
+		long stale = System.currentTimeMillis() - TIMEOUT;
+		if (connections != null && !connections.isEmpty()) {
+			for (JDCConnection conn : connections) {
+				if ((conn.inUse()) && (stale > conn.getLastUse())
+						&& (!conn.validate())) {
+					removeConnection(conn);
+				}
+			}
+		}
+	}
 
-    while ((connlist != null) && (connlist.hasMoreElements())) {
-      JDCConnection conn = connlist.nextElement();
+	public synchronized void closeConnections() {
+		if (connections != null && !connections.isEmpty()) {
+			for (JDCConnection conn : connections) {
+				removeConnection(conn);
+			}
+		}
+	}
 
-      if ((conn.inUse()) && (stale > conn.getLastUse()) && (!conn.validate())) {
-        removeConnection(conn);
-      }
-    }
-  }
+	private synchronized void removeConnection(JDCConnection conn) {
+		connections.remove(conn);
+	}
 
-  public synchronized void closeConnections() {
-    Enumeration<JDCConnection> connlist = connections.elements();
+	public synchronized Connection getConnection() throws SQLException {
+		JDCConnection c;
 
-    while ((connlist != null) && (connlist.hasMoreElements())) {
-      JDCConnection conn = connlist.nextElement();
-      removeConnection(conn);
-    }
-  }
+		for (int i = 0; i < connections.size(); i++) {
+			c = connections.get(i);
+			if (c.lease()) {
+				return c;
+			}
+		}
 
-  private synchronized void removeConnection(JDCConnection conn) {
-    connections.removeElement(conn);
-  }
+		Connection conn = DriverManager.getConnection(url, user, password);
+		c = new JDCConnection(conn, this);
+		c.lease();
+		connections.add(c);
 
-  public synchronized Connection getConnection() throws SQLException {
-    JDCConnection c;
+		return c.getConnection();
+	}
 
-    for (int i = 0; i < connections.size(); i++) {
-      c = connections.elementAt(i);
-      if (c.lease()) {
-        return c;
-      }
-    }
-
-    Connection conn = DriverManager.getConnection(url, user, password);
-    c = new JDCConnection(conn, this);
-    c.lease();
-    connections.addElement(c);
-
-    return c.getConnection();
-  }
-
-  public synchronized void returnConnection(JDCConnection conn) {
-    conn.expireLease();
-  }
+	public synchronized void returnConnection(JDCConnection conn) {
+		conn.expireLease();
+	}
 }
 
 class ConnectionReaper extends Thread {
-  private ConnectionService pool;
-  private final long delay = 300000;
+	private ConnectionService pool;
+	private static final long DELAY = 300000;
 
-  ConnectionReaper(ConnectionService pool) {
-    this.pool = pool;
-  }
+	ConnectionReaper(ConnectionService pool) {
+		this.pool = pool;
+	}
 
-  @Override
-  public void run() {
-    while (true) {
-      try {
-        Thread.sleep(delay);
-      } catch (InterruptedException e) {
-      }
-      pool.reapConnections();
-    }
-  }
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(DELAY);
+			} catch (InterruptedException e) {
+			}
+			pool.reapConnections();
+		}
+	}
 }
