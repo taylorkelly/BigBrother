13b1e8413242f5f4b27ad62239b41c920708609f
Daniel Chesters <daniel.chesters@gmail.com>
2011-02-20 21:05:49 +0100

    Oups forgotten save

--- src/main/java/me/taylorkelly/bigbrother/datablock/ButtonPress.java
+++ src/main/java/me/taylorkelly/bigbrother/datablock/ButtonPress.java
@@ -6,7 +6,6 @@ import org.bukkit.block.Block;
 public class ButtonPress extends BBDataBlock {
 
 	public ButtonPress(String player, Block button, int world) {
-		//TODO Better world support
 		super(player, Action.BUTTON_PRESS, world, button.getX(), button.getY(), button.getZ(), 77, Byte.toString(button.getData()));
 	}
 
