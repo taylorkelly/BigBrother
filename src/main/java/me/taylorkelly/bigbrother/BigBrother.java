c433b15d0dd8629b6c3d728ee5688c9316a966ca
GpMidi <paul@gpmidi.net>
2011-02-26 15:42:38 -0500

    - Reverting Fix3 removal

--- src/main/java/me/taylorkelly/bigbrother/BigBrother.java
+++ src/main/java/me/taylorkelly/bigbrother/BigBrother.java
@@ -28,6 +28,7 @@ import me.taylorkelly.bigbrother.datasource.DataBlockSender;
 import me.taylorkelly.bigbrother.finder.Finder;
 import me.taylorkelly.bigbrother.finder.Sticker;
 import me.taylorkelly.bigbrother.fixes.Fix;
+import me.taylorkelly.bigbrother.fixes.Fix3;
 import me.taylorkelly.bigbrother.fixes.Fix13;
 import me.taylorkelly.bigbrother.fixes.Fix14;
 import me.taylorkelly.bigbrother.griefcraft.util.Updater;
@@ -132,6 +133,8 @@ public class BigBrother extends JavaPlugin {
         fix.apply();
         Fix fix2 = new Fix14(getDataFolder());
         fix2.apply();
+        Fix fix3 = new Fix3(getDataFolder()); // 26 Feb
+        fix3.apply();
 
         // Initialize Permissions, Stats
         BBPermissions.initialize(getServer());
