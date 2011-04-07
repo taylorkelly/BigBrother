12e94c48383e1c11d9c8aa4606ec50919110cddf
Benjamin Grosse <benjamin@web3.es>
2011-04-07 16:34:02 +0200

    make queries postgresql compatible
    do_addOrUpdatePlayer does not use ON DUPLICATE... for postsgresql anymore

--- src/main/java/me/taylorkelly/bigbrother/tablemgrs/BBDataPostgreSQL.java
+++ src/main/java/me/taylorkelly/bigbrother/tablemgrs/BBDataPostgreSQL.java
@@ -5,4 +5,41 @@ public class BBDataPostgreSQL extends BBDataMySQL {
     public static String getMySQLIgnore() {
         return " ";
     }
+    
+    /* (non-Javadoc)
+     * @see me.taylorkelly.bigbrother.tablemgrs.DBTable#onLoad()
+     */
+    @Override
+    public void onLoad() {
+        
+    }
+    
+    /* (non-Javadoc)
+     * @see me.taylorkelly.bigbrother.tablemgrs.DBTable#getCreateSyntax()
+     */
+    @Override
+    public String getCreateSyntax() {
+    	return "CREATE TABLE \""+getTableName()+"\" ("
+        + "\"id\" SERIAL,"
+        + "\"date\" INT NOT NULL DEFAULT '0'," 
+        + "\"player\" INT NOT NULL DEFAULT 0," 
+        + "\"action\" smallint NOT NULL DEFAULT '0'," 
+        + "\"world\" smallint NOT NULL DEFAULT '0'," 
+        + "\"x\" int NOT NULL DEFAULT '0'," 
+        + "\"y\" smallint NOT NULL DEFAULT '0'," 
+        + "\"z\" int NOT NULL DEFAULT '0'," 
+        + "\"type\" smallint NOT NULL DEFAULT '0',"
+        + "\"data\" varchar(500) NOT NULL DEFAULT '',"
+        + "\"rbacked\" boolean NOT NULL DEFAULT '0',"
+        + "PRIMARY KEY (\"id\"))";
+    	/*, 
+        INDEX(\"world\"), 
+        INDEX(\"x\",\"y\",\"z\"), 
+        INDEX(\"player\"),
+        INDEX(\"action\"), 
+        INDEX(\"date\"), 
+        INDEX(\"type\"), 
+        INDEX(\"rbacked\")*/
+    }
+    
 }
